7b63606487a5db452e07778578c15b04e336b654
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-07-02 17:57:50 -0400

    frontend: modified header content position; added max width and padding to body

--- frontend/src/components/private_header.js
+++ frontend/src/components/private_header.js
@@ -9,26 +9,29 @@ export default (props) =>{
             {user =>{
                 return (
                     <nav className='private-nav'>
-                        <h1 className='app-title'>Hello, {user.name}</h1>
+                        <div className='private-nav-container'>
+                            <div className='app-title-container'>
+                                <h1 style={{fontSize:'25pt'}}>Hello, {user.name}</h1>
+                            </div>
 
-                        <ul className='private-nav-list'>
-                            <Link to='/' className='link'>
-                                <li className='list-item'>Portfolio</li>
-                            </Link>
+                            <ul className='private-nav-list'>
+                                <Link to='/' className='link'>
+                                    <li className='list-item'>Portfolio</li>
+                                </Link>
 
-                            <li className='list-item pipe'>|</li>
-                            
-                            <Link to='/transactions' className='link'>
-                                <li className='list-item'>Transactions</li>
-                            </Link>
-                            
-                            <li className='list-item pipe'>|</li>
-                            
-                            <Link to='/logout' className='link'>
-                                <li className='list-item'>Log Out</li>
-                            </Link>
-                        </ul>
-
+                                <li className='list-item pipe'>|</li>
+                                
+                                <Link to='/transactions' className='link'>
+                                    <li className='list-item'>Transactions</li>
+                                </Link>
+                                
+                                <li className='list-item pipe'>|</li>
+                                
+                                <Link to='/logout' className='link'>
+                                    <li className='list-item'>Log Out</li>
+                                </Link>
+                            </ul>
+                        </div>
                     </nav>
                 )
             }}
--- frontend/src/components/public_header.js
+++ frontend/src/components/public_header.js
@@ -6,15 +6,20 @@ import './styling/public_header.css';
 export default (props) =>{
     return (
         <nav className='nav'>
-            <h1 className='app-title'>Your Stock Portfolio App</h1>
-            <ul className='nav-list'>
-                <Link to='/' className='link'>
-                    <li className='list-item'>Login</li>
-                </Link>
-                <Link to='/signup' className='link'>
-                    <li className='list-item'>Signup</li>
-                </Link>
-            </ul>
+            <div className='nav-container'>
+                <div className='app-title-container'>
+                    <h1 style={{fontSize:'25pt'}}>Your Stock Portfolio App</h1>
+                </div>
+
+                <ul className='nav-list'>
+                    <Link to='/' className='link'>
+                        <li className='list-item'>Login</li>
+                    </Link>
+                    <Link to='/signup' className='link'>
+                        <li className='list-item'>Signup</li>
+                    </Link>
+                </ul>
+            </div>
         </nav>
     )
 }
