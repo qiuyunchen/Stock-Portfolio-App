ae6517a0cd702005288e5cac70322cdb39a75182
Qiuyun Chen <qiuyunchen@pursuit.org>
2019-07-02 00:06:48 -0400

    frontend: added private header

--- frontend/src/components/private_header.js
+++ frontend/src/components/private_header.js
@@ -1,6 +1,37 @@
 import React from 'react';
-import firebase from '../firebase';
+import {Link} from 'react-router-dom';
+import {AuthContext} from '../contexts';
+import './styling/private_header.css';
 
 export default (props) =>{
-    return <div>private header</div>
+    return (
+        <AuthContext.Consumer>
+            {user =>{
+                return (
+                    <nav className='private-nav'>
+                        <h1 className='app-title'>Hello, {user.name}</h1>
+
+                        <ul className='private-nav-list'>
+                            <Link to='/' className='link'>
+                                <li className='list-item'>Portfolio</li>
+                            </Link>
+
+                            <li className='list-item pipe'>|</li>
+                            
+                            <Link to='/transactions' className='link'>
+                                <li className='list-item'>Transactions</li>
+                            </Link>
+                            
+                            <li className='list-item pipe'>|</li>
+                            
+                            <Link to='/logout' className='link'>
+                                <li className='list-item'>Log Out</li>
+                            </Link>
+                        </ul>
+
+                    </nav>
+                )
+            }}
+        </AuthContext.Consumer>
+    )
 }
